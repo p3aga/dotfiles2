89934925aeb3b3ea478d9fca74d3e7275e75949c
Pedro Henrick <pedrohenrickqa@gmail.com>
2026-09-09 17:56:59 -0300

    feat: create script for install packages & configure services

--- .chezmoiscripts/run_once_before_brew-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if command -v "brew" > /dev/null 2>&1; then
-  echo "Installing Homebrew"
-  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
